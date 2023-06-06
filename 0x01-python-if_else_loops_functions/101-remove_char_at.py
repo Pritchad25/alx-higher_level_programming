e0a1efb38bc8353e9562abd41822adece05dc160
Pritchad25 <pricts29@gmail.com>
2023-06-06 12:22:20 -0700

    Correction of python coding style.

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    """This function creates a copy of the string without the character at position n."""
+    """creates a copy of the string without the character at position n."""
     new = ""
     i = 0
     for c in str:
