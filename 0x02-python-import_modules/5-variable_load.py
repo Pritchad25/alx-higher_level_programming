d4c4e4654f554a41049175ea9d2c21f388f40660
Pritchad25 <pricts29@gmail.com>
2023-06-08 12:33:36 -0700

    Update on file.

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -4,5 +4,9 @@ if __name__=="__main__":
     """This program prints the value of a variable a from variable_load_5."""
     from variable_load_5 import a
 
-    print(a)
+    def find_var():
+        print(a)
+
+    if __name__ == "__main__":
+        find_var()
 
