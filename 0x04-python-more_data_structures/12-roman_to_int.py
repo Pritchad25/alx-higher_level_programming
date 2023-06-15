5bb12b1222719c7beffd226478112cffabad5959
Pritchad25 <pricts29@gmail.com>
2023-06-15 08:55:35 -0700

    Change of typo that caused errors to output.

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -17,7 +17,7 @@ def roman_to_int(roman_string):
     if not roman_string:
         return 0
 
-    rom_n = {'l': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     list_keys = list(rom_n.keys())
 
     num = 0
