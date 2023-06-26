6786eef0b61bf0b5807984819d295f7e6e42c842
Pritchad25 <pricts29@gmail.com>
2023-06-26 08:37:03 -0700

    addition of one more exception message

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def list_divisison(my_list_1, my_list_2, list_length):
+def list_division(my_list_1, my_list_2, list_length):
     """This function divides 2 lists element
     by element
 
@@ -16,16 +16,16 @@ def list_divisison(my_list_1, my_list_2, list_length):
     newList = []
     for i in range(0, list_length):
         try:
-            divList = my_list_1[i] / my_list_2[i]
+            div = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
-            divList = 0
+            div = 0
         except ZeroDivisionError:
             print("division by 0")
-            divList = 0
+            div = 0
         except IndexError:
             print("out of range")
-            divList = 0
+            div = 0
         finally:
-            newList.append(divList)
+            newList.append(div)
     return (newList)
