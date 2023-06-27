438345153e85036573462b076802c9c608055b66
Pritchad25 <pricts29@gmail.com>
2023-06-27 07:24:24 -0700

    Addition of docstrings on file.

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""Defines an empty class called Square"""
 
 class Square:
-    python3 -c 'print(__import__("my_module").MyClass.__doc__)'
     """This class defines a square"""
     pass
