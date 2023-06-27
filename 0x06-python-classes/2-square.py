86cf47e1374cc7225ab4e630bc4f1964b0369d83
Pritchad25 <pricts29@gmail.com>
2023-06-27 08:05:05 -0700

    A class that defines a square by using a private instance attribute.

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""the following code defines a class called Square"""
+
+class Square:
+    """Defines a square by using a private instance
+    attribute. No module is imported.
+    """
+
+    def __init__(self, size=0):
+        """This init method initializes a new Square.
+        No module is imported.
+
+        Args:
+            size (int): defines the size of the new square.
+        """
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
