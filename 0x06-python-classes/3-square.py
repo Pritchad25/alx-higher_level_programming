34e9bec3796db33ae364f0043c6dfa211f6fc0f5
Pritchad25 <pricts29@gmail.com>
2023-06-27 09:44:22 -0700

    Addition of appropriate docstrings for documentation.

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,12 +15,11 @@ class Square:
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise Value Error("size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
         """This public instance method returns the
         current area of the square.
-
         """
         return (self.__size * self.__size)
