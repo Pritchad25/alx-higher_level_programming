a54c7522d09f5ddbb14ce033429dcbd974ae95eb
Pritchad25 <pricts29@gmail.com>
2023-08-16 00:48:53 -0700

    A script that displays the average temperature by city ordered by temperature(descending).

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,7 @@
+-- Temperatures #0
+-- A script that displays the average temperature by
+-- city ordered by temperature in descending order.
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
