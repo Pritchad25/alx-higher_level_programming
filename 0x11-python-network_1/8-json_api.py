84bbd00be0048df3665758c573e62277a6f0c542
Pritchad25 <pricts29@gmail.com>
2023-10-01 07:52:32 -0700

    A PYthon script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This Python script takes in a letter and sends a POST request to
+a URL with the letter as a parameter.
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+
+    res = requests.post(url, data={'q': q})
+
+    try:
+        res = res.json()
+
+        if res:
+            print("[{}] {}".format(res.get('id'), res.get('name')))
+        else:
+            print("No result")
+    except Exception:
+        print('Not a valid JSON')
